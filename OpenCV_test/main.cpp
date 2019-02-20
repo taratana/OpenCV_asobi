9081f76ab69593fc8c6fee3cbba8248e55d510f6
Mato <3stepda@gmail.com>
2019-02-21 04:06:13 +0900

    根の長さ計測

--- OpenCV_test/main.cpp
+++ OpenCV_test/main.cpp
@@ -7,36 +7,39 @@ using namespace cv;
 using namespace std;
 
 int main() {
-	Mat image = imread("seed.jpg", IMREAD_UNCHANGED);
+	Mat image = imread("root.jpg", IMREAD_UNCHANGED);
+	imshow("IMAGE", image);
 	Mat grayScaleImage;
 	cvtColor(image, grayScaleImage, COLOR_BGR2GRAY);
 	
 	//二値化
 	Mat binImage;
-	//threshold(grayScaleImage, binImage, 0, 255, THRESH_BINARY | THRESH_OTSU);
-	adaptiveThreshold(grayScaleImage, binImage, 255,  ADAPTIVE_THRESH_MEAN_C,THRESH_BINARY_INV,99,15);
-	
-	//収縮処理
-	Mat element = Mat::ones(3, 3, CV_8UC1);
-	erode(binImage, binImage, element, Point(-1, -1), 3);
-	
-	//輪郭抽出
-	vector<vector<Point>> contours;
-	findContours(binImage, contours, RETR_EXTERNAL, CHAIN_APPROX_NONE);
-	for (int i = 0; i<contours.size(); i++) {
-		int count = contours[i].size();
-		double x = 0, y = 0;
-		for (int j = 0; j < count; j++) {
-			x += contours[i][j].x;
-			y += contours[i][j].y;
-		}
-		x /= count;
-		y /= count;
-		circle(image, Point(x, y), 3, Scalar(0, 0, 255), 2, 4);
-	}
-	cout << contours.size() << " seeds" << endl;
+	//threshold(grayScaleImage, binImage, 0, 255, THRESH_BINARY_INV | THRESH_OTSU);
+	adaptiveThreshold(grayScaleImage, binImage, 255,  ADAPTIVE_THRESH_MEAN_C,THRESH_BINARY_INV,45,15);
+	Mat binImageClone = binImage.clone();
+	imshow("BINIMAGE", binImage);
 
-	imshow("IMAGE", image);	
+	//全体の根の長さ計測
+	vector<vector<Point>> contours;
+	findContours(binImage, contours, RETR_LIST, CHAIN_APPROX_NONE);
+	double coutourLenAll = 0;
+	for (int i = 0; i < contours.size(); i++) {
+		coutourLenAll += arcLength(contours[i], 1);
+	}
+	cout << "estimated root length(total)=" << coutourLenAll / 2 << endl;
+
+	//主根の長さ計測
+	erode(binImageClone, binImageClone, Mat(), Point(-1, -1), 4);
+	findContours(binImageClone, contours, RETR_LIST, CHAIN_APPROX_NONE);
+	double contourLenMain = 0;
+	for (int i = 0; i < contours.size(); i++) {
+		contourLenMain += arcLength(contours[i], 1);
+	}
+	cout << "estimated root length(main)=" << contourLenMain / 2 << endl;
+	cout << "estimated root length(lateral)=" << (coutourLenAll-contourLenMain) / 2 << endl;
+	imshow("Eroded",binImageClone);
+
+
 	imwrite("outputImage.png", image);
 	waitKey(0);
 	return 0;
